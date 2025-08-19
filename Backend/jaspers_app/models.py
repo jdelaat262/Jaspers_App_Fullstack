7c57075ac8d3c461b78dfd9ac087acbbb80a1ef5
jasper <jdelaat1@gmail.com>
2025-08-19 14:21:34 +0200

    MySQL up and running, nog geen communicatie tussen front en back

--- Backend/jaspers_app/models.py
+++ Backend/jaspers_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    
+    # De __str__ methode geeft een leesbare naam
+    # van de objecten in de Django Admin
+    def __str__(self):
+        return self.name
